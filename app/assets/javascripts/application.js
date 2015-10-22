b6a4b89257f53cfa292b2fc6d68afbbaa92f15f3
toreserter <toreserter@gmail.com>
2015-10-22 22:07:45 +0300

    new ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
 
 autosave_timeout = null
